4f0579f6ff577867e8f02d89d69e4285d4dc2591
UDUAK 'EREN <uduakeren@users.noreply.github.com>
2016-08-06 00:25:37 -0700

    Created StaffList and StudentList components

---
+++ public/components/StaffList.js
@@ -0,0 +1,82 @@
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Staff from "../components/Staff"
+
+
+export default React.createClass({
+
+	getInitialState(){
+		return {
+			staff: this.props
+		};
+	},
+
+
+	render(){
+
+		var {staff} = this.props; 
+
+		var renderMessage = () => {
+			if (staff.length == 0){
+
+				return(
+					<div>
+						<p>No staff details found. You can use the form above to add the staff</p>
+					</div>
+				);
+
+			}
+			else {
+				return(
+					<div>
+				        <div class="row">
+							<div class="col-sm-2 col-lg-2 ccol md-2">
+								<b>First Name</b>
+							</div>
+							<div class="col-sm-2 col-lg-2 col md-2">
+								<b>Last Name</b>
+							</div>
+							<div class="col-sm-2 col-lg-2 col md-2">
+								<b>Role</b>
+							</div>
+							<div class="col-sm-6 col-lg-6 col md-6">
+								<b>Manager</b>
+							</div>
+						</div>
+						<br />
+			            {renderStaffs()}
+		            </div>
+	            );
+			}
+		};
+
+		var renderStaffs = () => {
+			return staff.map((staffMember) => {
+				return (
+					<Staff key={staffMember.id} onEdit={this.props.onEditStaff} onDelete={this.props.onDeleteStaff} 
+					{...staffMember}/>
+				);
+			});
+		};
+
+		return(
+			<div>
+				<div class="container">
+			        <p class="line-breaker" />
+			        <div class="row row-header">
+			        	<p class="staff-header">Staff Assignment</p>
+			        	<br />
+			        
+				        <div class="well width-well">
+				        	{renderMessage()}
+				        </div>
+			        </div>
+			    </div>
+			</div>
+		);
+	}
+
+});
---
+++ public/components/StudentList.js
@@ -0,0 +1,82 @@
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Student from "../components/Student"
+
+
+export default React.createClass({
+
+	getInitialState(){
+		return {
+			students: this.props
+		};
+	},
+
+
+	render(){
+
+		var {students} = this.props; 
+
+		var renderMessage = () => {
+			if (students.length == 0){
+
+				return(
+					<div>
+						<p>No student details found. You can use the form above to add the student</p>
+					</div>
+				);
+
+			}
+			else {
+				return(
+					<div>
+				        <div class="row">
+							<div class="col-sm-2 col-lg-2 ccol md-2">
+								<b>First Name</b>
+							</div>
+							<div class="col-sm-2 col-lg-2 col md-2">
+								<b>Last Name</b>
+							</div>						
+							<div class="col-sm-2 col-lg-2 col md-2">
+								<b>Grade</b>
+							</div>							
+							<div class="col-sm-6 col-lg-6 col md-6">
+								<b>School</b>
+							</div>
+						</div>
+						<br />
+			            {renderStudents()}
+		            </div>
+	            );
+			}
+		};
+
+		var renderStudents = () => {
+			return students.map((student) => {
+				return (
+					<Student key={student.id} onEdit={this.props.onEditStudent} onDelete={this.props.onDeleteStudent} 
+					{...student}/>
+				);
+			});
+		};
+
+		return(
+			<div>
+				<div class="container">
+			        <p class="line-breaker" />
+			        <div class="row row-header">
+			        	<p class="student-header">Student Assignment</p>
+			        	<br />
+			        
+				        <div class="well width-well">
+				        	{renderMessage()}
+				        </div>
+			        </div>
+			    </div>
+			</div>
+		);
+	}
+
+});
