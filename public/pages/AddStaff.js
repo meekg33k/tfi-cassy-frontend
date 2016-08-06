94436b9c2af4dd2165b3ab4af8cdbaca34a2341b
UDUAK 'EREN <uduakeren@users.noreply.github.com>
2016-08-06 00:21:29 -0700

    Refactoring AddStaff from static to use components

--- public/pages/AddStaff.js
+++ public/pages/AddStaff.js
@@ -1,75 +1,136 @@
 "use strict"
 
+import uuid from "node-uuid";
 import React from "react";
 import ReactDOM from "react-dom";
-import Search from "../components/Search"
+
+import AddStaffForm from "../components/AddStaffForm"
+import StaffList from "../components/StaffList"
+import Search from "../components/Searcher"
+import SearchProcessor from "../../apis/Helper"
+
 
 
 export default React.createClass({
+
+	getInitialState(){
+		return {
+			searchString: "",
+			staff: [
+				{
+					id: 123,
+					firstName: "Christy",
+					lastName: "Hayes",
+					role: "Executive",
+					manager: "Christy Hayes"
+
+				}, {
+					id: 456,
+					firstName: "Eve",
+					lastName: "Jackson",
+					role: "Administrator",
+					manager: "Christy Hayes"
+
+				}, {
+					id: 789,
+					firstName: "John",
+					lastName: "Doe",
+					role: "Program Manager",
+					manager: "Eve Jackson"
+
+				},
+			]
+		};
+	},
+
+	handleAddStaff(staff){
+
+		this.setState({
+			staff: [
+				...this.state.staff,
+				staff
+			]
+		});
+	},
+
+
+	handleDeleteStaff(staff){
+
+		this.state.staff.map((staffer) => {
+			console.log(staffer.id);
+
+		});
+
+		var victimId;
+
+	    if (confirm("Do you want to proceed to delete the staff?") == true) {
+
+	    	for(var i = 0;  i < this.state.staff.length; i++) {
+			    if (this.state.staff[i].id === staff.id) {
+			        victimId = i;
+			        break;
+			    }
+			}
+
+			var updatedStaff = this.state.staff.splice(victimId, 1);
+
+			this.setState(updatedStaff);
+	    }
+
+	},
+
+
+	handleEditStaff(editedStaff){
+
+		var updatedStaff = this.state.staff.map((staffMember) => {
+			if (staffMember.id === editedStaff.id){
+				staffMember.firstName = editedStaff.firstName;
+				staffMember.lastName = editedStaff.lastName;
+				staffMember.role = editedStaff.role;
+				staffMember.manager = editedStaff.manager;
+			};
+		});
+
+		this.setState(updatedStaff);
+
+		this.state.staff.map((staffMember) => {
+			console.log(staffMember.firstName);
+		});
+	},
+
+
+	handleSearch(searchString){
+		this.setState({
+			searchString: searchString.toLowerCase()
+		});
+	},
+
   	render() {
+
+  		var {staff, searchString} = this.state;
+
+  		var filteredStaff = SearchProcessor.filter(staff, searchString);
+
 	    return (
 	    	<div>
 				<div class="container">
 			        <p class="line-breaker" />
 			        <div class="row row-header">
-			            <div class="col-xs-12 col-sm-12 col-lg-12 ccol md-12">
-			                <p class="section-header">Add Staff</p>
+			            <div class="col-xs-12 col-sm-7 col-lg-7 ccol md-7">
+			                <p class="staff-header">Add Staff Member</p>
+			            </div>
+			            <div class="col-xs-12 col-sm-5 col-lg-5 col md-5 ">
+			            	<Search onSearch={this.handleSearch} placeholder = "Enter staff name here to search"/>
 			            </div>
 			        </div>
 			        <div class="row row-header report-form">
 			            <p class="line-breaker" />
-			            <form class="form-horizontal" role="form">
-			                <div class="form-group">
-			                    <label for="firstName" class="col-sm-2 control-label">First Name</label>
-			                    <div class="col-sm-5">
-			                      <input type="text" class="form-control" id="firstName" placeholder="" />
-			                    </div>
-			                </div>
-			                <div class="form-group">
-			                    <label for="lastName" class="col-sm-2 control-label">Last Name</label>
-			                    <div class="col-sm-5">
-			                      <input type="text" class="form-control" id="lastName" placeholder="" />
-			                    </div>
-			                </div>
-			                <div class="form-group">
-			                    <label for="email" class="col-sm-2 control-label">Email Address</label>
-			                    <div class="col-sm-5">
-			                      <input type="email" class="form-control" id="email" placeholder="" />
-			                    </div>
-			                </div>
-			                <div class="form-group">
-			                    <label for="role" class="col-sm-2 control-label">Role</label>
-			                    <div class="col-sm-5">
-			                        <select class="form-control" id="role">
-			                            <option>Administrator</option>
-			                            <option>Program Manager</option>
-			                            <option>Site Coordinator</option>
-			                            <option>Therapist</option>
-			                        </select>
-			                    </div>
-			                </div>
-			                <div class="form-group">
-			                    <label for="manager" class="col-sm-2 control-label">Manager</label>
-			                    <div class="col-sm-5">
-			                        <select class="form-control" id="role">
-			                            <option>Eve Johnson</option>
-			                            <option>John Doe</option>
-			                            <option>Jill Smith</option>
-			                        </select>
-			                    </div>
-			                </div>
-			                <p class="line-breaker"></p>
-			                <div class="form-group">
-			                    <div class="col-sm-offset-2 col-sm-10">
-			                        <button type="submit" class="btn btn-danger">Add Staff</button>
-			                    </div>
-			                </div>
-			            </form>
+			            <AddStaffForm onAddStaff={this.handleAddStaff}/>
+			            <p class="line-breaker" />
+			            <StaffList staff={filteredStaff} onEditStaff={this.handleEditStaff} onDeleteStaff={this.handleDeleteStaff}/>
 			        </div>
 			    </div>
 			</div>
 		);
   }
 });
-
-	
