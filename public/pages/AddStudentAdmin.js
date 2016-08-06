4b8b2e95b34f417ae006aaca7a12b5d4ebf1447b
UDUAK 'EREN <uduakeren@users.noreply.github.com>
2016-08-06 00:22:16 -0700

    Refactoring AddStudent use case from static to use components

--- public/pages/AddStudentAdmin.js
+++ public/pages/AddStudentAdmin.js
@@ -1,92 +1,121 @@
 "use strict"
 
+import uuid from "node-uuid";
 import React from "react";
 import ReactDOM from "react-dom";
 
+import AddStudentForm from "../components/AddStudentForm"
+import StudentList from "../components/StudentList"
+import Search from "../components/Searcher"
+import SearchProcessor from "../../apis/Helper"
+
+
 
 export default React.createClass({
+
+	getInitialState(){
+		return {
+			searchString: "",
+			students: [
+				{
+					id: 898091,
+					firstName: "Jermain",
+					lastName: "Dupril",
+					gender: "Male",
+					grade: "5th",
+					school: "Loyola College",
+					ethnicity: "African American"
+				}, {
+					id: 777118,
+					firstName: "Priyanka",
+					lastName: "Miya",
+					gender: "Female",
+					grade: "12th",
+					school: "Ranswood School",
+					ethnicity: "Asian Indian"
+				},
+			]
+		};
+	},
+
+	handleAddStudent(student){
+
+		this.setState({
+			students: [
+				...this.state.students,
+				student
+			]
+		});
+	},
+
+
+	handleDeleteStudent(student){
+
+		var victimId;
+
+	    if (confirm("Do you want to proceed to delete the student?") == true) {
+
+	    	for(var i = 0;  i < this.state.students.length; i++) {
+			    if (this.state.students[i].id === student.id) {
+			        victimId = i;
+			        break;
+			    }
+			}
+
+			var updatedStudents = this.state.students.splice(victimId, 1);
+
+			this.setState(updatedStudents);
+	    }
+
+	},
+
+
+	handleEditStudent(editedStudent){
+
+		var updatedStudents = this.state.students.map((student) => {
+			if (student.id === editedStudent.id){
+				student.firstName = editedStudent.firstName;
+				student.lastName = editedStudent.lastName;
+				student.grade = editedStudent.grade;
+				student.school = editedStudent.school;
+				student.gender = editedStudent.gender;
+			};
+		});
+
+		this.setState(updatedStudents);
+
+	},
+
+
+	handleSearch(searchString){
+		this.setState({
+			searchString: searchString.toLowerCase()
+		});
+	},
+
   	render() {
+
+  		var {students, searchString} = this.state;
+
+  		var filteredStudents = SearchProcessor.filter(students, searchString);
+
 	    return (
 	    	<div>
-				<div class="container">
-			        <p class="line-breaker" />
-			        <div class="row row-header">
-			            <div class="col-xs-12 col-sm-12 col-lg-12 ccol md-12">
-			                <p class="student-header">Add Student</p>
+				 <div class="container">
+                    <p class="line-breaker" />
+                    <div class="row row-header">
+                        <div class="col-xs-12 col-sm-7 col-lg-7 col md-7">
+                            <p class="student-header">Add Student</p>
+                        </div>
+			            <div class="col-xs-12 col-sm-5 col-lg-5 ccol md-5 ">
+			            	<Search onSearch={this.handleSearch} placeholder = "Enter student name here to search"/>
 			            </div>
 			        </div>
 			        <div class="row row-header report-form">
-			            <p>Use this form to add new student to the CASSY system</p>
 			            <p class="line-breaker" />
-			            <form class="form-horizontal" role="form">
-			                <div class="form-group">
-			                    <label for="studentName" class="col-sm-2 control-label">Student Name</label>
-			                    <div class="col-sm-5">
-			                      <input type="text" class="form-control" id="studentName" placeholder="" />
-			                    </div>
-			                </div>
-			                <div class="form-group">
-			                    <label for="grade" class="col-sm-2 control-label">Grade</label>
-			                    <div class="col-sm-5">
-			                        <select class="form-control" id="grade">
-			                            <option>Kindergarten</option>
-			                            <option>1st</option>
-			                            <option>2nd</option>
-			                            <option>3rd</option>
-			                            <option>4th</option>
-			                            <option>5th</option>
-			                            <option>6th</option>
-			                            <option>7th</option>
-			                            <option>8th</option>
-		                              	<option>9th</option>
-			                            <option>10th</option>
-			                            <option>11th</option>
-			                            <option>12th</option>
-			                        </select>
-			                    </div>
-			                </div>
-			                <div class="form-group">
-			                    <label for="school" class="col-sm-2 control-label">School</label>
-			                    <div class="col-sm-5">
-			                        <select class="form-control" id="eventType">
-			                            <option>Ranswood Elementary</option>
-			                            <option>ABC HighSchool</option>
-			                            <option>XYZ College</option>
-			                        </select>
-			                    </div>
-			                </div>
-			                <div class="form-group">
-			                    <label for="gender" class="col-sm-2 control-label">Gender</label>
-			                    <div class="col-sm-5">
-			                        <select class="form-control" id="gender">
-			                            <option>Male</option>
-			                            <option>Female</option>
-			                            <option>Not specified</option>
-			                        </select>
-			                    </div>
-			                </div>
-			                <div class="form-group">
-			                    <label for="ethnicity" class="col-sm-2 control-label">Ethnicity</label>
-			                    <div class="col-sm-5">
-			                        <select class="form-control" id="ethnicity">
-			                            <option>Caucasian</option>
-			                            <option>Hispanic/Latino</option>
-			                            <option>Asian</option>
-			                            <option>African American</option>
-			                            <option>Pacific Islander</option>
-		                              	<option>Native American</option>
-			                            <option>Asian Indian</option>
-			                     		<option>Mixed Race</option>
-			                        </select>
-			                    </div>
-			                </div>
-			                <p class="line-breaker"></p>
-			                <div class="form-group">
-			                    <div class="col-sm-offset-2 col-sm-10">
-			                        <button type="submit" class="btn btn-success">Add Student</button>
-			                    </div>
-			                </div>
-			            </form>
+			            <AddStudentForm onAddStudent={this.handleAddStudent}/>
+			            <p class="line-breaker" />
+			            <StudentList students={filteredStudents} onEditStudent={this.handleEditStudent} onDeleteStudent={this.handleDeleteStudent}/>
 			        </div>
 			    </div>
 			</div>
