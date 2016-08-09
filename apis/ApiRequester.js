8b63b3d965307edac348579617c0739aa13a69ee
UDUAK 'EREN <uduakeren@users.noreply.github.com>
2016-08-08 19:16:15 -0700

    Adding createUser method to server API calls

--- apis/ApiRequester.js
+++ apis/ApiRequester.js
@@ -4,9 +4,8 @@ import axios from "axios";
 
 var CASSY_URL;
 
-///const CASSY_WEBSERV_URL = "https://cassy-server.herokuapp.com/";
 if (process.env.NODE_ENV === "production"){
-	CASSY_URL = "https://cassydataportal.herokuapp.com/";
+	CASSY_URL = "https://cassy-server.herokuapp.com/";
 }
 else {
 	CASSY_URL = "http://localhost:8888/";
@@ -19,10 +18,26 @@ module.exports = {
 		console.log(CASSY_URL);
 
 		return axios.post(CASSY_URL+"login", {
-			username: "batman", 
+			username: "batman",
 			password: "mobile"
 		}).then(function(res) {
 
+			if (res.data.cod && res.data.message){
+				throw new Error(res.data.message);
+			}
+			else {
+				console.log(res.data);
+				return res.data;
+			}
+		}, function(res) {
+			throw new Error(res.data.message);
+		});
+	},
+
+	createNewUser: function(user){
+		console.log(CASSY_URL);
+
+		return axios.post(CASSY_URL+"users", user).then(function(res) {
 			if (res.data.cod && res.data.message){
 				throw new Error(res.data.message);
 			}
