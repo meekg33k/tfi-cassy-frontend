4d523a50a755f5fc87336849bac2ac9bb04f4fac
UDUAK 'EREN <uduakeren@users.noreply.github.com>
2016-08-12 16:12:47 -0700

    Creating School landing page and SchoolList components

---
+++ public/components/SchoolList.js
@@ -0,0 +1,78 @@
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import School from "../components/School"
+
+
+export default React.createClass({
+
+	getInitialState(){
+		return {
+			schools: this.props
+		};
+	},
+
+
+	render(){
+
+		var {schools} = this.props;
+
+		console.log(schools);
+
+		var renderMessage = () => {
+			if (schools.length == 0){
+				return(
+					<div>
+						<p>No school found. You can use the form above to add the school</p>
+					</div>
+				);
+
+			}
+			else {
+				return(
+					<div>
+		        		<div class="row">
+							<div class="col-sm-2 col-lg-2 col md-2">
+								<b>School Name</b>
+							</div>
+							<div class="col-sm-3 col-lg-3 col md-3">
+								<b>Contact Email</b>
+							</div>
+							<div class="col-sm-1 col-lg-1 col md-1">
+								<b>District</b>
+							</div>
+							<div class="col-sm-6 col-lg-6 col md-6">
+								<b>Site Coordinator</b>
+							</div>
+						</div>
+						<br />
+            			{renderSchools()}
+          			</div>
+          		);
+			}
+		};
+
+		var renderSchools = () => {
+			return schools.map((school) => {
+				return (
+					<School key={school.id} onEdit={this.props.onEditSchool} onDelete={this.props.onDeleteSchool}
+					{...school}/>
+				);
+			});
+		};
+
+		return(
+			<div>
+				<div class="container">
+			        <div class="row row-header">
+				        <div class="well width-well">
+				        	{renderMessage()}
+				        </div>
+			        </div>
+			    </div>
+			</div>
+		);
+	}
+});
---
+++ public/pages/School.js
@@ -0,0 +1,131 @@
+"use strict"
+
+import React from "react"
+import ReactDOM from "react-dom"
+import BreadCrumb from "react-breadcrumbs"
+import {connect} from "react-redux"
+
+import AddSchoolForm from "../components/AddSchoolForm" 
+import SchoolList from "../components/SchoolList"
+import Search from "../components/Searcher"
+import SearchProcessor from "../../apis/Helper"
+
+import * as actions from "../../actions/actions"
+
+
+var School =  React.createClass({
+
+	getInitialState(){
+		return {
+      		addSchool: this.props,
+			searchString: "",
+			schools: this.props
+		};
+	},
+
+
+	handleAddSchool(school){
+		//To include REST API calls
+		var { dispatch } = this.props;
+		dispatch(actions.addSchool(school));
+		this.handleExitAddSchool();
+	},
+
+
+	handleDeleteSchool(school){
+
+	    if (confirm("Do you want to proceed to delete the school?") == true) {
+	    	//TODO: Include REST API calls to delete school
+	    	var { dispatch } = this.props;
+			dispatch(actions.deleteSchool(school));
+	    }
+
+	},
+
+
+	handleEditSchool(editedSchool){
+
+		var { dispatch } = this.props;
+		dispatch(actions.editSchool(editedSchool));
+	},
+
+
+	handleExitAddSchool(){
+		var { dispatch } = this.props;
+		dispatch(actions.enableAddSchool(false));
+	},
+
+
+	initiateAddSchool(){
+		var { dispatch } = this.props;
+		dispatch(actions.enableAddSchool(true));
+	},
+
+
+	handleSearch(searchString){
+		this.setState({
+			searchString: searchString.toLowerCase()
+		});
+	},
+
+
+  	render() {
+
+  		var {schools} = this.props;
+  		var searchString= this.state.searchString;
+  		var filteredSchools = SearchProcessor.filterEvents(schools, searchString);
+
+			var renderAddSchool = () =>{
+				if (this.props.addSchool){
+					return(
+						<div>
+							<AddSchoolForm onAddSchool={this.handleAddSchool} onExitAddSchool={this.handleExitAddSchool}/>
+						</div>
+					);
+				}
+
+			};
+
+	    return (
+	    	<div>
+				<div class="container">
+			        <p class="line-breaker" />
+					<BreadCrumb routes={this.props.routes} separator =" >> "/>
+					<br />
+					<br />
+			        <div class="row row-header report-form">
+						{renderAddSchool()}
+						<br />
+						<div class="row row-header">
+		                    <div class="col-xs-12 col-sm-6 col-lg-6 col-md-6">
+		                        <p class="school-header">School List</p>
+		                    </div>
+		                    <div class="col-xs-12 col-sm-2 col-lg-2 col-md-2">
+		                      	<div class="form-group">
+		                            <button type="submit" class="btn btn-primary" onClick={this.initiateAddSchool}>
+		                              <span class="glyphicon glyphicon-plus" aria-hidden="true">  </span>
+		                            &nbsp; Add School
+		                          </button>
+		                      	</div>
+		                    </div>
+	  			            <div class="col-xs-12 col-sm-4 col-lg-4 col-md-4">
+	  			            	<Search onSearch={this.handleSearch} placeholder = "Enter school name here to search"/>
+	  			            </div>
+	                  	</div>
+			            <SchoolList schools={filteredSchools} onEditSchool={this.handleEditSchool} onDeleteSchool={this.handleDeleteSchool}/>
+			        </div>
+			    </div>
+			</div>
+			);
+  	}
+});
+
+module.exports = connect(
+	(store) => {
+		return {
+			schools: store.schools,
+			addSchool: store.addSchoolState
+		};
+	}
+)(School);
+
