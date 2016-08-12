ffd05d0f9e6e8a1b752aad9dfdfc11b94d1e8794
UDUAK 'EREN <uduakeren@users.noreply.github.com>
2016-08-12 11:37:01 -0700

    Temporary changes to server side route

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
 var apiManager = require('../services/api-manager');
+var apiMailgun = require('../services/mailgun');
 
 // GET all users
 router.get('/', (req, res, next) => {
-  res.send('a list of all users');
+  apiManager.allUsers((err, result) => {
+    if (err) {
+      console.error(`Error getting all users ${err}`);
+    }
+
+    res.status(200).send(result);
+  });
 });
 
 // GET user by userid
@@ -13,7 +20,7 @@ router.get('/:userid', (req, res, next) => {
     if (err) {
       console.error('No user for the given id ' + req.params.userid + err);
     }
-    
+
     res.status(200).send(result);
   });
 });
@@ -24,8 +31,33 @@ router.post('/', (req, res, next) => {
     if (err) {
       console.error('Error creating a new user' + err);
     }
-    
+
     console.log('User inserted with id ' + result.insertId);
+
+    // Get created user details for sending email
+    apiManager.getUser(result.insertId, (err, user) => {
+      if (err) {
+        console.error('No user for the given id ' + result.insertId + err);
+      }
+
+      // Construct email data object
+      var data = {
+        from: 'Admin <admin@cassyapp.com>',
+        to: 'anup.kher.1990@gmail.com',
+        subject: 'Hello from Cassy',
+        text: `You have been entered into the system. Your default password is ${user[0].password}`
+      };
+
+      // Send email with default password
+      apiMailgun.sendMail(data, (err, body) => {
+        if (err) {
+          console.error('There was an error sending the email: ' + err);
+        }
+
+      });
+
+    });
+
     res.status(201).send(result);
   });
 });
@@ -36,7 +68,7 @@ router.put('/:userid', (req, res, next) => {
     if (err) {
       console.error('Could not update user with id ' + req.params.userid + err);
     }
-    
+
     console.log('User updated');
     res.status(201).send('Updated ' + result.changedRows + ' rows');
   });
@@ -48,7 +80,7 @@ router.put('/:userid/change-password', (req, res, next) => {
     if (err) {
       console.error('Error changing user\'s password ' + req.params.userid + err);
     }
-    
+
     console.log('Password updated');
     res.status(200).send('Updated ' + result.changedRows + ' rows');
   });
@@ -60,7 +92,7 @@ router.delete('/:userid', (req, res, next) => {
     if (err) {
       console.error('Error deleting user with id ' + req.params.id);
     }
-  
+
     res.status(200).send('Deleted ' + result.affectedRows + ' rows');
   });
 });
