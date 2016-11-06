813f52989b64c76afb80687b40e83314bcd0e748
UDUAK 'EREN <uduakeren@users.noreply.github.com>
2016-11-06 11:25:05 -0800

    Fixing bugs in navbar

--- public/components/NavBarIndex.js
+++ public/components/NavBarIndex.js
@@ -2,6 +2,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
+import { IndexLink, Link } from "react-router";
 
 //class Layout extends React.Component {
 export default React.createClass({
